94d39ee30406d141efde72add861f200df497062
Eemeli Lottonen <eemed@protonmail.com>
2022-12-06 19:43:51 +0200

    graphemes

--- buffer/src/piece_tree/graphemes.rs
+++ buffer/src/piece_tree/graphemes.rs
@@ -3,6 +3,9 @@ use crate::cursor_iterator::CursorIterator;
 use super::{Bytes, PieceTree};
 use bstr::ByteSlice;
 
+// Using bstr to convert bytes to grapheme clusters.
+// bstr is not meant to be used on streaming 
+
 #[inline]
 fn is_utf8_start(byte: u8) -> bool {
     (byte & 0xc0) != 0x80
@@ -21,28 +24,80 @@ fn is_utf8_start(byte: u8) -> bool {
 //         false
 //     }
 // }
+//
+
+#[inline]
+fn read_next_codepoint(bytes: &mut Bytes, buf: &mut Vec<u8>) -> bool {
+    match bytes.get() {
+        Some(b) => buf.push(b),
+        None => return false,
+    }
+
+    while let Some(b) = bytes.next() {
+        if is_utf8_start(b) {
+            break;
+        }
+
+        buf.push(b);
+    }
+
+    true
+}
 
 pub fn next_grapheme_boundary(pt: &PieceTree, pos: usize) -> Option<usize> {
     let mut bytes = Bytes::new(pt, pos);
     let mut buf = Vec::new();
+    let mut prev_match = None;
 
     loop {
-        if let Some((_start, end, _grapheme)) = buf.grapheme_indices().next() {
-            return Some(pos + end);
-        } else {
-            buf.push(bytes.get()?);
+        if !read_next_codepoint(&mut bytes, &mut buf) {
+            return prev_match.map(|p| pos + p);
+        }
 
-            let mut byte = bytes.next()?;
-            while !is_utf8_start(byte) {
-                buf.push(byte);
-                byte = bytes.next()?;
+        if let Some((start, end, grapheme)) = buf.grapheme_indices().next() {
+            let end_matches_prev = prev_match.map_or(false, |prev| prev == end);
+            if end_matches_prev {
+                return Some(pos + end);
             }
+
+            prev_match = Some(end);
         }
     }
 }
 
+#[inline]
+fn read_prev_codepoint(bytes: &mut Bytes, buf: &mut Vec<u8>) -> bool {
+    let len = buf.len();
+    while let Some(b) = bytes.prev() {
+        buf.insert(0, b);
+
+        if is_utf8_start(b) {
+            break;
+        }
+    }
+
+    len != buf.len()
+}
+
 pub fn prev_grapheme_boundary(pt: &PieceTree, pos: usize) -> Option<usize> {
-    todo!()
+    let mut bytes = Bytes::new(pt, pos);
+    let mut buf = Vec::new();
+    let mut prev_match_len = None;
+
+    loop {
+        if !read_prev_codepoint(&mut bytes, &mut buf) {
+            return prev_match_len.map(|p| pos - p);
+        }
+
+        if let Some((start, end, grapheme)) = buf.grapheme_indices().next_back() {
+            let end_matches_prev = prev_match_len.map_or(false, |prev| prev == end - start);
+            if end_matches_prev {
+                return Some(pos - (end - start));
+            }
+
+            prev_match_len = Some(end - start);
+        }
+    }
 }
 
 pub fn next_grapheme(pt: &PieceTree, pos: usize) -> Option<(usize, usize, String)> {
@@ -57,31 +112,56 @@ pub fn prev_grapheme(pt: &PieceTree, pos: usize) -> Option<(usize, usize, String
 mod test {
     use super::*;
 
-    #[test]
-    fn next_grapheme() {
-        let mut pt = PieceTree::new();
-        pt.insert_str(0, "foobar");
+    // #[test]
+    // fn next_grapheme() {
+    //     let mut pt = PieceTree::new();
+    //     pt.insert_str(0, "foobar");
 
-        println!(
-            "Next grapheme boundary: {:?}",
-            next_grapheme_boundary(&pt, 0)
-        );
-    }
+    //     println!(
+    //         "Next grapheme boundary: {:?}",
+    //         next_grapheme_boundary(&pt, 0)
+    //     );
+    // }
 
     #[test]
-    fn next_grapheme_multi_byte() {
+    fn next_grapheme_boundary_multi_byte() {
         let mut pt = PieceTree::new();
-        const CONTENT: &str = "❤🤍🥳❤️";
+        const CONTENT: &str = "❤🤍🥳❤️간÷나는산다⛄";
         pt.insert_str(0, CONTENT);
 
         let mut start = 0;
 
         while let Some(end) = next_grapheme_boundary(&pt, start) {
             println!(
-                "Next grapheme boundary multibyte: {:?}",
+                "Next grapheme boundary multibyte: {}, {}",
+                end,
                 &CONTENT[start..end]
             );
             start = end;
         }
     }
+
+    #[test]
+    fn prev_grapheme_boundary_multi_byte() {
+        let mut pt = PieceTree::new();
+        const CONTENT: &str = "❤🤍🥳❤️간÷나는산다⛄";
+        pt.insert_str(0, CONTENT);
+
+        let mut start = CONTENT.len();
+
+        println!("content: {:?}", CONTENT.as_bytes());
+        while let Some(end) = prev_grapheme_boundary(&pt, start) {
+            println!(
+                "Prev grapheme boundary multibyte: {end}..{start} {}",
+                &CONTENT[end..start],
+            );
+            start = end;
+        }
+    }
+
+    // #[test]
+    // fn next_grapheme_test() {
+    //     const CONTENT: &[u8] = "❤️".as_bytes();
+    //     println!("TEST {:?}", CONTENT.grapheme_indices().next());
+    // }
 }

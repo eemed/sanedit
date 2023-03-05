33407e9eee1cb5c39c60726652436be88a7b4f39
Eemeli Lottonen <eemed@protonmail.com>
2023-03-05 14:19:59 +0200

    string cursor

--- regex/src/cursor.rs
+++ regex/src/cursor.rs
@@ -4,3 +4,38 @@ pub trait CharCursor {
     fn next(&mut self) -> Option<char>;
     fn prev(&mut self) -> Option<char>;
 }
+
+pub(crate) struct StringCursor<'a> {
+    pos: usize,
+    string: &'a str,
+}
+
+impl<'a> StringCursor<'a> {
+    pub fn new(string: &str) -> StringCursor {
+        StringCursor { string, pos: 0 }
+    }
+}
+
+impl<'a> CharCursor for StringCursor<'a> {
+    fn at_start(&self) -> bool {
+        self.pos == 0
+    }
+
+    fn at_end(&self) -> bool {
+        self.pos == self.string.len()
+    }
+
+    fn next(&mut self) -> Option<char> {
+        let part = &self.string[self.pos..];
+        let ch = part.chars().next()?;
+        self.pos += ch.len_utf8();
+        Some(ch)
+    }
+
+    fn prev(&mut self) -> Option<char> {
+        let part = &self.string[..self.pos];
+        let ch = part.chars().rev().next()?;
+        self.pos -= ch.len_utf8();
+        Some(ch)
+    }
+}

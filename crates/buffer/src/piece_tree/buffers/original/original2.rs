f591cf34c70c434edb9a866d8486e88335872c5a
Eemeli Lottonen <eemed@protonmail.com>
2023-09-18 19:58:01 +0300

    hmm

--- crates/buffer/src/piece_tree/buffers/original/original2.rs
+++ crates/buffer/src/piece_tree/buffers/original/original2.rs
@@ -1,15 +1,42 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, io};
 
 enum Block {
-    Mmap {},
-    File {},
-    Memory {},
+    // File { },
+    Mmap { map: memmap::Mmap },
+    Memory { bytes: Vec<u8> },
 }
 
-struct Blocks {
-    map: BTreeMap<usize, Block>,
-}
+type Blocks = BTreeMap<usize, Block>;
 
 struct OriginalBuffer {
     blocks: Blocks,
+}
+
+impl OriginalBuffer {
+    #[inline]
+    pub fn new() -> OriginalBuffer {
+        OriginalBuffer {
+            blocks: Blocks::new(),
+        }
+    }
+
+    #[inline]
+    pub fn from_reader<T: io::Read>(mut reader: T) -> io::Result<OriginalBuffer> {
+        let mut bytes = Vec::new();
+        reader.read_to_end(&mut bytes)?;
+        let block = Block::Memory { bytes };
+        let mut blocks = Blocks::new();
+        blocks.insert(0, block);
+        Ok(OriginalBuffer { blocks })
+    }
+
+    /// Returns the length of the original buffer
+    pub fn len(&self) -> usize {
+        0
+    }
+
+    /// Returns the length of the backing file if file backed
+    pub fn file_len(&self) -> usize {
+        0
+    }
 }
--- crates/buffer/src/piece_tree/write.rs
+++ crates/buffer/src/piece_tree/write.rs
@@ -1,16 +1,30 @@
-use std::{ops::Range, path::Path};
+use std::{io, ops::Range, path::Path};
 
-use crate::ReadOnlyPieceTree;
+use crate::{piece_tree::buffers::BufferKind, ReadOnlyPieceTree};
 
-use super::tree::piece::Piece;
+use super::tree::{
+    piece::{self, Piece},
+    pieces::Pieces,
+};
 
-enum WriteOperation {
-    Extend(usize),
-    Truncate(usize),
-    Overwrite { source: Piece, target: Range<usize> },
+enum WriteOp {
+    ExtendFileTo { size: usize },
+    TruncateFileTo { size: usize },
+    Overwrite,
 }
 
-pub fn write_to(path: &Path, pt: &ReadOnlyPieceTree, tmp: &Path) {
+struct Overwrite {
+    source: Piece,
+    target: Range<usize>,
+}
+
+pub fn write_in_place(pt: &ReadOnlyPieceTree) -> io::Result<()> {
+    if !pt.is_file_backed() {
+        todo!()
+    }
+
+    todo!()
+
     // If pt.orig file backed
     // => dependency graph
     // => to write operations
@@ -20,4 +34,24 @@ pub fn write_to(path: &Path, pt: &ReadOnlyPieceTree, tmp: &Path) {
     //
     //
     // piece -> orig
+}
+
+fn calculate_write_operations(pt: &ReadOnlyPieceTree) -> Vec<WriteOp> {
+    let mut pcs = Vec::new();
+    let mut pieces = Pieces::new(pt, 0);
+    while let Some((pos, piece)) = pieces.next() {
+        let target = pos..pos + piece.len;
+        //
+        // if piece is from original buffer and
+        //
+        // if piece.kind == BufferKind::Original {
+        // }
+
+        pcs.push(Overwrite {
+            source: piece,
+            target,
+        })
+    }
+
+    todo!()
 }

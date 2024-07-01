a921c34992f3bbe57d5a32581c8056a0b8d722e7
Eemeli Lottonen <eemed@protonmail.com>
2024-07-01 20:59:06 +0300

    remove scroll

--- crates/editor/src/editor/windows/window/filetree.rs
+++ crates/editor/src/editor/windows/window/filetree.rs
@@ -1,14 +1,10 @@
 #[derive(Debug)]
 pub(crate) struct FiletreeView {
     pub(crate) selection: usize,
-    pub(crate) scroll: usize,
 }
 
 impl Default for FiletreeView {
     fn default() -> Self {
-        FiletreeView {
-            selection: 0,
-            scroll: 0,
-        }
+        FiletreeView { selection: 0 }
     }
 }

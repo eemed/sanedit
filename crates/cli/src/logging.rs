423e55872250a0c8d8c89e3e76176b5e58357464
Eemeli Lottonen <eemed@protonmail.com>
2024-09-30 20:12:47 +0300

    logger start

--- crates/cli/src/logging.rs
+++ crates/cli/src/logging.rs
@@ -1,10 +1,16 @@
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::Write;
 use std::ops::Deref;
 use std::panic;
+use std::path::PathBuf;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 use log::LevelFilter;
-use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Config, Logger, Root};
-use log4rs::encode::pattern::PatternEncoder;
+// use log4rs::append::file::FileAppender;
+// use log4rs::config::{Appender, Config, Logger, Root};
+// use log4rs::encode::pattern::PatternEncoder;
 
 const LOG_FILE: &str = "/tmp/sanedit.log";
 
@@ -34,26 +40,57 @@ pub fn setup(debug: bool) {
         log::error!("A panic occurred at {}:{}: {}", filename, line, cause);
     }));
 
-    let level = if debug {
-        LevelFilter::Debug
-    } else {
-        LevelFilter::Info
-    };
+        let level= if debug {
+            LevelFilter::Debug
+        } else {
+            LevelFilter::Info
+        };
 
-    let file_appender = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{l} {d(%H:%M:%S.%3f)} {f}:{L} {m}{n}",
-        )))
-        .build(LOG_FILE)
-        .unwrap();
+        let ignore = vec!["regex_cursor", "grep_regex", "globset"]
+            .into_iter()
+            .map(String::from)
+            .collect();
+    let logger = Logger::new(level, LOG_FILE, ignore);
 
-    let config = Config::builder()
-        .appender(Appender::builder().build("file-appender", Box::new(file_appender)))
-        .logger(Logger::builder().build("regex_cursor", LevelFilter::Off))
-        .logger(Logger::builder().build("grep_regex", LevelFilter::Off))
-        .logger(Logger::builder().build("globset", LevelFilter::Off))
-        .build(Root::builder().appender("file-appender").build(level))
-        .unwrap();
+    logger.init();
+}
 
-    let _handle = log4rs::init_config(config).unwrap();
+struct Logger {
+    level: LevelFilter,
+    output_file: Rc<RefCell<File>>,
+    ignore_crates: Vec<String>,
+}
+
+impl Logger {
+    pub fn new(level: LevelFilter, path: &str, ignore: Vec<String>) -> Logger {
+    }
+
+    pub fn init(self) {
+        log::set_max_level(self.level);
+        let _ = log::set_boxed_logger(Box::new(self));
+    }
+
+    fn is_ignored(&self, module: &str) -> bool {
+        for c in &self.ignore_crates {
+            if c == module {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        metadata.level() <= self.level && !self.is_ignored(metadata.target())
+    }
+
+    fn log(&self, record: &log::Record) {
+        todo!()
+    }
+
+    fn flush(&self) {
+        self.output_file.flush()
+    }
 }

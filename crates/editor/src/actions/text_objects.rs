bc2ee3ad4b79941ea71fc6633f19fe474b9eb212
Eemeli Lottonen <eemed@protonmail.com>
2024-10-16 21:30:21 +0300

    fix select line

--- crates/editor/src/actions/text_objects.rs
+++ crates/editor/src/actions/text_objects.rs
@@ -12,12 +12,40 @@ use sanedit_server::ClientId;
 use super::hooks;
 
 fn select_range(editor: &mut Editor, id: ClientId, start: &str, end: &str, include: bool) {
-    select_impl(editor, id, |slice, pos| {
+    select(editor, id, |slice, pos| {
         find_range(slice, pos, start, end, include)
     });
 }
 
-fn select_impl<F: Fn(&PieceTreeSlice, u64) -> Option<BufferRange>>(
+fn select_with_col<F: Fn(&PieceTreeSlice, u64) -> Option<(BufferRange, usize)>>(
+    editor: &mut Editor,
+    id: ClientId,
+    f: F,
+) {
+    let mut changed = false;
+    let (win, buf) = editor.win_buf_mut(id);
+    let slice = buf.slice(..);
+
+    for cursor in win.cursors.cursors_mut() {
+        let pos = cursor.pos();
+        let range = (f)(&slice, pos);
+
+        if let Some((range, col)) = range {
+            if !range.is_empty() {
+                cursor.select(&range);
+                cursor.set_column(col);
+                changed = true;
+            }
+        }
+    }
+
+    if changed {
+        win.view_to_cursor(buf);
+        hooks::run(editor, id, Hook::CursorMoved);
+    }
+}
+
+fn select<F: Fn(&PieceTreeSlice, u64) -> Option<BufferRange>>(
     editor: &mut Editor,
     id: ClientId,
     f: F,
@@ -46,13 +74,13 @@ fn select_impl<F: Fn(&PieceTreeSlice, u64) -> Option<BufferRange>>(
 
 #[action("Select line")]
 fn select_line(editor: &mut Editor, id: ClientId) {
-    select_impl(editor, id, |slice, pos| {
+    select_with_col(editor, id, |slice, pos| {
         let start = movement::start_of_line(&slice, pos);
         let end = next_line_start(&slice, pos);
         if start == end {
             None
         } else {
-            Some(Range::new(start, end))
+            Some((Range::new(start, end), 0))
         }
     });
 }
@@ -129,10 +157,10 @@ fn select_backtick(editor: &mut Editor, id: ClientId) {
 
 #[action("Select word under cursor")]
 fn select_word(editor: &mut Editor, id: ClientId) {
-    select_impl(editor, id, word_at_pos);
+    select(editor, id, word_at_pos);
 }
 
 #[action("Select paragraph under cursor")]
 fn select_paragraph(editor: &mut Editor, id: ClientId) {
-    select_impl(editor, id, paragraph_at_pos);
+    select(editor, id, paragraph_at_pos);
 }

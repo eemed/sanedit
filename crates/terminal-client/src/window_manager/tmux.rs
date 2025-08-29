e588ca76d97fccca87134007f278a006040436bf
Eemeli Lottonen <eemed@protonmail.com>
2025-08-29 21:04:33 +0300

    dont close spawned windows, stay at shell

--- crates/terminal-client/src/window_manager/tmux.rs
+++ crates/terminal-client/src/window_manager/tmux.rs
@@ -7,12 +7,12 @@ pub(crate) struct Tmux {}
 
 impl TerminalWindowManager for Tmux {
     fn new_window_horizontal(&mut self, shell: &str, new_instance_cmd: &str) {
-        let split = format!("tmux split-window '{new_instance_cmd}'");
+        let split = format!("tmux split-window {shell} -c '{new_instance_cmd}; exec {shell}'");
         let _ = Command::new(shell).args(["-c", &split]).output();
     }
 
     fn new_window_vertical(&mut self, shell: &str, new_instance_cmd: &str) {
-        let split = format!("tmux split-window -h '{new_instance_cmd}'");
+        let split = format!("tmux split-window -h {shell} -c '{new_instance_cmd}; exec {shell}'");
         let _ = Command::new(shell).args(["-c", &split]).output();
     }
 }

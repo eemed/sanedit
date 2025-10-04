5a9b782e4502bc9b5837c4f5a1b2e47d02ca709f
Eemeli Lottonen <eemed@protonmail.com>
2025-10-04 15:44:05 +0300

    fix some syntax

--- crates/core/src/char.rs
+++ crates/core/src/char.rs
@@ -263,7 +263,7 @@ impl DisplayOptions {
         use Replacement::*;
 
         const DEFAULT: [(Replacement, char); 7] = [
-            (Tab, '→'),
+            (Tab, '⭢'),
             (TabFill, ' '),
             (EOL, ' '),
             (BufferEnd, '~'),
